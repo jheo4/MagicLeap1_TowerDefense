03c0a32a701d7a09730ea5e842171973b5e2a8d4
Jin Heo <993jin@gmail.com>
2022-11-01 22:18:48 +0900

    feat: add enemy script & prefab

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody enemyRigidbody;
+    public float speed = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidbody = GetComponent<Rigidbody>();
+        enemyRigidbody.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+        Destroy(gameObject, 0);
+    }
+}
