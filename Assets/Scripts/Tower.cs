8171d35d448bb0795747ceeded707ef1697b1099
Jin Heo <993jin@gmail.com>
2022-11-01 22:25:28 +0900

    feat: add tower prefabs with script

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public bool installed;
+    public bool grapped;
+    public bool atStorage = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        installed = false;    
+        grapped = false;    
+        atStorage = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
