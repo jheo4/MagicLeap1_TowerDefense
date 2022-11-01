5834d362388266b1a0824d1064c8d6529943421f
Jin Heo <993jin@gmail.com>
2022-11-01 22:20:58 +0900

    feat: add start & end points with spawning/endpoint scripts

---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.Die();
+                // Need to reduce Life
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject endPoint;
+    public GameObject enemyPrefab;
+    public float spwanRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+    private Transform endPointLocation;
+    private float spawnRate;
+    private float timeAfterSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpawn = 0;
+        spawnRate = Random.Range(spwanRateMin, spawnRateMax);
+        endPointLocation = endPoint.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+        if(timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+            GameObject newEnemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+            newEnemy.transform.LookAt(endPointLocation);
+            spawnRate = Random.Range(spwanRateMin, spawnRateMax);
+        }
+    }
+}
