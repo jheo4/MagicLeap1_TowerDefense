01ab8bdbcf7c0e2f2f671bbe315499f10ab616f5
Jin Heo <993jin@gmail.com>
2022-11-03 13:56:53 +0900

    feat: add bullet tower scripts & prefab

---
+++ Assets/Scripts/BulletTower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTower : Tower
+{
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackSpeed = 0.4f;
+        attackRange = 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterAttack += Time.deltaTime;
+
+        if(target != null)
+        {
+            if(timeAfterAttack >= attackSpeed)
+            {
+                timeAfterAttack = 0f;
+                GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+                bullet.transform.LookAt(target.transform);
+            }
+        }
+
+        target = null;
+        findEnemyToAttack();
+    }
+}
